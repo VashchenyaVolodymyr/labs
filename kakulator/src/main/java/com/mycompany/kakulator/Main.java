079bf20b41cc53ddae9c542b290f31552c73624b
VashchenyaVolodymyr <vashvol322@gmail.com>
2020-11-26 21:38:52 +0200

    first commit

---
+++ kakulator/src/main/java/com/mycompany/kakulator/Main.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.kakulator;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author sasha
+ */
+public class Main {
+    public static void main(String[] args)  {
+                Scanner in = new Scanner(System.in);
+                System.out.print("A =  ");
+                double A = in.nextDouble();
+                System.out.print("B = ");
+                double B = in.nextDouble();
+                System.out.print("Mod =  ");
+                double mod = in.nextDouble();
+
+                System.out.print("For + : ");
+                System.out.println((A + B) % mod);
+                System.out.print("For - : ");
+                System.out.println((A - B) % mod);
+                System.out.print("For / : ");
+                System.out.println((A / B) % mod);
+                System.out.print("For * :");
+                System.out.println((A * B) % mod);
+                System.out.print("For ^ : ");
+                System.out.println((Math.pow(A, B)) % mod);
+        }
+}
